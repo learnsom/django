721149b94bf331c78c050781aa1b8fccf7e6c6b6
learnsom <166076409+learnsom@users.noreply.github.com>
2024-04-05 09:21:07 +1030

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,7 @@
+{
+  "name": "Learnsom",
+  "website": "https://twitter.com/learnsom",
+  "description": "Learn something new.",
+  "logo": "https://raw.githubusercontent.com/learnsom/django/main/tests/files/magic.png",
+  "twitter": "https://twitter.com/learnsom"
+}
